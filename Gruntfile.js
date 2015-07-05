fefc6bba87e8fd76b90ddf7b4b01809a2785191f
Oleg Borodai <borodai.oleg@gmail.com>
2015-07-05 22:59:15 +0200

    feat: smal changes to grunt, and add build directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -102,7 +102,6 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
